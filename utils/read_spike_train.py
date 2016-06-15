611701f5b006fc5a091c5722737d9129f870744e
ppuliu <liuhonglei@gmail.com>
2016-06-15 09:13:39 -0700

    add function to read spike trains

---
+++ utils/read_spike_train.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def read_spike_train(file_name):
+
+    spike_times=[]
+    max_time=0
+    with open(file_name) as f:
+        for line in f:
+            entries=map(int, line.split(','))
+            print len(entries)
+            print entries[-1]
+            if entries[-1]>max_time:
+                max_time=entries[-1]
+            spike_times.append(entries)
+    n=len(spike_times)
+    spike_train=np.zeros([n, max_time+1])
+
+    for i in xrange(n):
+        for j in spike_times[i]:
+            spike_train[i,j]=1
+
+    return spike_train
+
+
